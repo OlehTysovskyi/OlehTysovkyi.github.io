bc08a6dbd81c044a2c7da7a2d274c7ffab9b4572
Oleh <oleg.tisovsky228@gmail.com>
2024-01-24 16:40:02 +0200

    Added some fixes

--- src/components/CatalogItem.tsx
+++ src/components/CatalogItem.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import closeIcon from '../assets/images/close.png';
 
 interface Props {
     image_url: string;
@@ -24,15 +25,18 @@ const CatalogItem = ({ image_url, name, price }: Props) => {
         <figcaption className="catalog-item-fig">
           <p className="catalog-item-name">{name}</p>
           <p className="catalog-item-price">{price}</p>
-          <button type="button" className="catalog-item-btn" onClick={openImage}>
+          <button type="button" className="catalog-item-btn-view" onClick={openImage}>
             Оглянути
           </button>
         </figcaption>
       </figure>
 
       {isImageOpen && (
-        <div className="fullscreen-overlay" onClick={closeImage}>
+        <div className="fullscreen-overlay">
           <div className="fullscreen-image-container">
+            <button type="button" className="catalog-item-btn-close" onClick={closeImage}>
+              <img src={closeIcon} width="27px" title="Закрити" alt="Закрити"/>
+            </button>
             <img
               src={image_url}
               alt={name}
